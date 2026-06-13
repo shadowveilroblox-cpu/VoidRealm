da07417badcdc37b25fff246180c55d81ec34430
shadowveilroblox-cpu <shadowveilroblox@gmail.com>
2026-06-13 23:25:08 +0600

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -169,12 +169,12 @@ export async function createInitialHelpMenu(client) {
 
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
+        .setURL("https://discord.gg/aUQrbbxrd")
         .setStyle(ButtonStyle.Link);
 
     const touchpointButton = new ButtonBuilder()
-        .setLabel("Learn from Touchpoint")
-        .setURL("https://www.youtube.com/@TouchDisc")
+        .setLabel("Creator Channel")
+        .setURL("https://www.youtube.com/@NilaBillaBloxYT")
         .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
